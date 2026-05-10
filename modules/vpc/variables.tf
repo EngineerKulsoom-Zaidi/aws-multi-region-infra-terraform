0ff43e3703327f780b8ff4b7b71befd454d15116
Kulsoom Zaidi <zaidikulsoom263@gmail.com>
2026-05-10 22:49:23 +0530

    Add VPC variables for region, CIDR, and name

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+}
